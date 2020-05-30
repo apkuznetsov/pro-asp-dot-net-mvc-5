1016a8539c398108ccbd6bf8543dafc1afccab75
Andrey <kouznts@ya.ru>
2020-05-30 17:31:03 +0400

    create post RsvpForm method

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -16,10 +14,23 @@ namespace PartyInvites.Controllers
 
             return View();
         }
-
+        
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            } else
+            {
+                return View();
+            }
         }
     }
 }
