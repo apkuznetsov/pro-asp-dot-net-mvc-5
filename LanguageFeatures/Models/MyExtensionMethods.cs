1321052fedac7633b2c0247456af655483488766
Andrey <kouznts@ya.ru>
2020-05-30 22:48:10 +0400

    add enumerable interface

--- LanguageFeatures/Models/MyExtensionMethods.cs
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -1,11 +1,13 @@
-﻿namespace LanguageFeatures.Models
+﻿using System.Collections.Generic;
+
+namespace LanguageFeatures.Models
 {
     public static class MyExtensionMethods
     {
-        public static decimal TotalPrices(this ShoppingCart cart)
+        public static decimal TotalPrices(this IEnumerable<Product> products)
         {
             decimal totalPrice = 0;
-            foreach (Product p in cart.Products)
+            foreach (Product p in products)
             {
                 totalPrice += p.Price;
             }
