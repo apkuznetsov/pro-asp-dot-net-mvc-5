8d243084bb4e5c87f3bf0b1bcb0a529304743990
Andrey <kouznts@ya.ru>
2020-05-30 22:14:53 +0400

    create MyExtensionMethods class

---
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this ShoppingCart cart)
+        {
+            decimal totalPrice = 0;
+            foreach (Product p in cart.Products)
+            {
+                totalPrice += p.Price;
+            }
+
+            return totalPrice;
+        }
+    }
+}
