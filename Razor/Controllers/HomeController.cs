6041f3a3f636910711a6928871a0c3d380dcb3fe
Andrey <kouznts@ya.ru>
2020-05-31 12:17:14 +0400

    create Index view

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -1,16 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using Razor.Models;
 using System.Web.Mvc;
 
 namespace Razor.Controllers
 {
     public class HomeController : Controller
     {
+        Product myProduct = new Product
+        {
+            ProductId = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
         public ActionResult Index()
         {
-            return View();
+            return View(myProduct);
         }
 
         public ActionResult About()
