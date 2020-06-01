f5bf01260efdba15cdd658231da99a6284ea6462
Andrey <kouznts@ya.ru>
2020-06-01 16:23:38 +0400

    create ShoppingCart model

---
+++ EssentialTools/Models/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace EssentialTools.Models
+{
+    public class ShoppingCart
+    {
+        private LinqValueCalculator calculator;
+
+        public ShoppingCart(LinqValueCalculator calculator)
+        {
+            this.calculator = calculator;
+        }
+
+        public IEnumerable<Product> Products { get; set; }
+
+        public decimal CalcTotalProductsPrice()
+        {
+            return calculator.CalcProductsPrice(Products);
+        }
+    }
+}
