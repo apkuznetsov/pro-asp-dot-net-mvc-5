6da0190a8bbd9331bb60f5ebac04f4d023464a0b
Andrey <kouznts@ya.ru>
2020-06-01 16:28:51 +0400

    create HomeController

--- EssentialTools/Controllers/HomeController.cs
+++ EssentialTools/Controllers/HomeController.cs
@@ -1,30 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using EssentialTools.Models;
 using System.Web.Mvc;
 
 namespace EssentialTools.Controllers
 {
     public class HomeController : Controller
     {
+        private Product[] products =
+        {
+            new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+            new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+            new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.5M},
+            new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M},
+        };
+
         public ActionResult Index()
         {
-            return View();
-        }
+            LinqValueCalculator calculator = new LinqValueCalculator();
+            ShoppingCart cart = new ShoppingCart(calculator) { Products = products };
+            decimal totalPrice = cart.CalcTotalProductsPrice();
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(totalPrice);
         }
     }
 }
