fccfd99842fdf58c51cbca906807341182947d29
Andrey <kouznts@ya.ru>
2020-06-01 16:19:50 +0400

    create CalcProductsPrice method

---
+++ EssentialTools/Models/LinqValueCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EssentialTools.Models
+{
+    public class LinqValueCalculator
+    {
+        public decimal CalcProductsPrice(IEnumerable<Product> products)
+        {
+            return products.Sum(p => p.Price);
+        }
+    }
+}
