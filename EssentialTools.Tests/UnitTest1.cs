edb28bb7042adacf98967f0c41078198ca5ab0c8
Andrey <kouznts@ya.ru>
2020-06-02 16:16:25 +0400

    create UnitTest1 class

--- EssentialTools.Tests/UnitTest1.cs
+++ EssentialTools.Tests/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using EssentialTools.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace EssentialTools.Tests
@@ -6,9 +6,20 @@ namespace EssentialTools.Tests
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IDiscountHelper GetTestObject()
         {
+            return new MinimumDiscountHelper();
+        }
+
+        [TestMethod]
+        public void Discount_Above_100()
+        {
+            IDiscountHelper target = GetTestObject();
+            decimal total = 200;
+
+            var discountedTotal = target.ApplyDiscount(total);
+
+            Assert.AreEqual(total * 0.9M, discountedTotal);
         }
     }
 }
