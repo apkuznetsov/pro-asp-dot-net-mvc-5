2e9d088c92935d882d2121edd8aca4363c1ecfdd
Andrey <kouznts@ya.ru>
2020-06-02 16:30:03 +0400

    create discount between 10 and 100 test

--- EssentialTools.Tests/UnitTest1.cs
+++ EssentialTools.Tests/UnitTest1.cs
@@ -21,5 +21,19 @@ namespace EssentialTools.Tests
 
             Assert.AreEqual(total * 0.9M, discountedTotal);
         }
+
+        [TestMethod]
+        public void Discount_Between_10_And_100()
+        {
+            IDiscountHelper target = GetTestObject();
+
+            decimal TenDollarDiscount = target.ApplyDiscount(10);
+            decimal HundredDollarDiscount = target.ApplyDiscount(100);
+            decimal FiftyDollarDiscount = target.ApplyDiscount(50);
+
+            Assert.AreEqual(5, TenDollarDiscount, "$10 dicount is wrong");
+            Assert.AreEqual(5, HundredDollarDiscount, "$100 dicount is wrong");
+            Assert.AreEqual(45, FiftyDollarDiscount, "$50 dicount is wrong");
+        }
     }
 }
