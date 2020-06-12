13a878297af9a907184447d9615cc2e2f2b27dc3
Andrey <kouznts@ya.ru>
2020-06-12 12:46:38 +0400

    create home controller

--- UrlsAndRoutes/Controllers/HomeController.cs
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace UrlsAndRoutes.Controllers
 {
@@ -10,21 +6,10 @@ namespace UrlsAndRoutes.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
+            ViewBag.Controller = "Home";
+            ViewBag.Action = "Index";
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View("ActionName");
         }
     }
 }
